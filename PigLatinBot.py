6b786181d7450d859e94f75f38961b17c614e25a
Matt Miller <mill1134@purdue.edu>
2014-09-30 14:33:49 -0400

    Update - ready for action

--- PigLatinBot.py
+++ PigLatinBot.py
@@ -4,9 +4,9 @@ import re
 r = praw.Reddit('Pig Latin Translator by /u/just_another_sheep v0.1. ')
 r.login('Pig_Bot', 'readfromtext')
 
-#main loop - look at posts from /r/test - this will be modified to /r/all
+#main loop - look at posts from /r/all - switch to /r/test for debugging
 
-all_comments = r.get_comments('test')
+all_comments = r.get_comments('all')
 #flat_comments = praw.helpers.flatten_tree(all_comments)
 
 #Maintain a list of comments that have been translated so we don't spam
@@ -17,26 +17,26 @@ PUNCTUATION = (".", ",", ":", "?", "!", ";")
 
 while True:
 
-  #evaluate comments without worrying about their rank
-  for comment in all_comments:
-  	
-  	#Find comments containing "Pig Latin"
-  	if "Pig Latin" in comment.body and comment.id not in already_done:
-  
-		  words = re.split(' ', comment.body)
-		  #words is a list of the words in the comment
-  		
-		  for entry in words:
-  			#translate into Pig Latin
+#evaluate comments without worrying about their rank
+	for comment in all_comments:
+	
+		#Find comments containing "Pig Latin"
+		if "Pig Latin" in comment.body and comment.id not in already_done:
 
-			  #if it starts with a vowel, just add -ay
-			  if entry[0] in VOWELS:
-  				our_comment += entry + "ay" + " "
-
-  			else:
-	  			our_comment += entry[1:] + entry[0] + "ay" + " "
+			words = re.split(' ', comment.body)
+			#words is a list of the words in the comment
 		
-		  #comment result
-		  comment.reply(our_comment)
-
-		  already_done.add(comment.id)
+			for entry in words:
+				#translate into Pig Latin
+	
+				#if it starts with a vowel, just add -ay
+				if entry[0] in VOWELS:
+					our_comment += entry + "ay" + " "
+	
+				else:
+					our_comment += entry[1:] + entry[0] + "ay" + " "
+			
+			#comment result
+			comment.reply(our_comment)
+	
+			already_done.add(comment.id)
